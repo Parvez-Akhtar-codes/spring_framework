2398ac9363c4667267a8fe7b4df45dfbf82ce0bf
Parvez-Akhtar-codes <parvezakhtar310@gmail.com>
2025-07-09 19:02:27 +0530

    commit

--- src/main/java/com/learning/spring_framework/AppGamingBasic02.java
+++ src/main/java/com/learning/spring_framework/AppGamingBasic02.java
@@ -13,6 +13,7 @@ public class AppGamingBasic02 {
 		//launch the spring context
 		var context = new AnnotationConfigApplicationContext(HelloWorldConfigrution.class);
 		System.out.println(context.getBean("name"));
+		System.out.println("for github repo");
 	}
 
 }
